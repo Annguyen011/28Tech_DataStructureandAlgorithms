4b3fe81c9fa652b17866313050a895a2a119affa
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-02-27 03:29:53 +0700

    BT_9

--- BT_9.cpp
+++ BT_9.cpp
@@ -1,21 +1,73 @@
 #include<iostream>
 #include<vector>
+#include<string>
 #include<algorithm>
 
 using namespace std;
 
 #define ll long long
 
-void Ql(int i);
+int n;
+int k;
+int a[1001];
+int ok;
 
+void KhoiTao();
+void Sinh();
 
 int main()
 {
+	cin >> k;
+
+	vector<string> v;
+
+	for (n = 1; v.size() < k; n++)
+	{
+		KhoiTao();
+
+		while (ok)
+		{
+			string s = "";
+			for (size_t i = 1; i <= n; i++)
+			{
+				s = s + to_string(a[i]);
+			}
+
+			for (size_t i = n; i >= 1; i--)
+			{
+				s = s + to_string(a[i]);
+			}
+			v.push_back(s);
+			Sinh();
+		}
+	}
 
 	return 0;
 }
 
-void Ql(int i)
+void KhoiTao()
 {
+	for (size_t i = 1; i <= n; i++)
+	{
+		a[i] = 1;
+	}
+}
 
+void Sinh()
+{
+	int i = n;
+	while (i >= 1 && a[i])
+	{
+		a[i] = 0;
+		i--;
+	}
+
+	if (i == 0)
+	{
+		ok = 0;
+	}
+	else
+	{
+		a[i] = 1;
+	}
 }
