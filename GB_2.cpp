1745cda48ac460410c4cd0f04fece2a83bfaa73a
Anngu <Anngu@MSI>
2024-02-26 14:30:18 +0700

    Add gb 2

---
+++ GB_2.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+
+using namespace std;
+
+int n;
+int lenght = 100;
+int a[1001];
+int ok = 0;
+
+void KhoiTao();
+void Sinh();
+
+int main()
+{
+	cin >> n;
+	KhoiTao();
+	while (!ok)
+	{
+		for (size_t i = 1; i <= n; i++)
+		{
+			if (a[i] == 0)
+			{
+				cout << "A";
+			}
+			else
+			{
+				cout << "B";
+			}
+		}
+		cout << endl;
+		Sinh();
+	}
+}
+
+
+// Tao ra cau hinh ban dau
+void KhoiTao()
+{
+	for (size_t i = 1; i <= n; i++)
+	{
+		a[i] = 0;
+	}
+}
+
+// Sinh ra cau hinh tiep theo
+void Sinh()
+{
+	int i = n;
+
+	while (i >= 1 && a[i] == 1)
+	{
+		a[i] = 0;
+		--i;
+	}
+
+	if (i == 0)
+	{
+		// Danh dau day la cau hinh cuoi cung
+		ok = 1;
+	}
+	else
+	{
+		a[i] = 1;
+	}
+}
