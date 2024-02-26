c96f1a723a0ec19318ee26cf7adf6002a69a4e91
Anngu <Anngu@MSI>
2024-02-26 16:15:25 +0700

    GB_5

--- GB_5.cpp
+++ GB_5.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 int n;
+int cnt;
 int a[1001];
 int ok = 0;
 
@@ -15,11 +16,14 @@ int main()
 {
 	cin >> n;
 	KhoiTao();
-	Sinh();
-
-	for (size_t i = 1; i <= n; i++)
+	while (!ok)
 	{
-		cout << a[i] << " ";
+		for (size_t i = 1; i <=  cnt; i++)
+		{
+			cout << a[i] << " ";
+		}
+		cout << endl;
+		Sinh();
 	}
 
 	return 0;
@@ -27,8 +31,10 @@ int main()
 
 void Sinh()
 {
-	int i = n - 1;
-	while (i >= 1 && a[i] > a[i + 1])
+	// Bat dau tu so hang cuoi cung
+	int i = cnt;
+
+	while (i >= 1 && a[i] == 1)
 	{
 		--i;
 	}
@@ -39,23 +45,32 @@ void Sinh()
 	}
 	else
 	{
-		for (size_t j = n; j > 0; j--)
-		{
-			if (a[i] < a[j])
-			{
-				swap(a[i], a[j]);
-				break;
-			}
-		}
+		a[i]--;
+		//So luong bu vao
+		int d = cnt - i + 1;
 
-		reverse(a + i + 1, a + n + 1);
+		cnt = i;
+		//So lan them lai phan tu bang gia tri a[i]
+		int q = d / a[i];
+		//Phan du neu co
+		int r = d % a[i];
+
+		for (int j = 1; j <= q; j++)
+		{
+			cnt++;
+			a[j + i] = a[i];
+		}
+		
+		if (r != 0)
+		{
+			++cnt;
+			a[cnt] = r;
+		}
 	}
 }
 
 void KhoiTao()
 {
-	for (size_t i = 1; i <= n; i++)
-	{
-		cin >> a[i];
-	}
+	cnt = 1;
+	a[1] = n;
 }
