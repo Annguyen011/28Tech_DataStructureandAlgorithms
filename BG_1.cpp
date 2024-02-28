5fae7e93a5bb5a745e073896056b1e4a2229a07d
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-02-28 14:57:53 +0700

    Add bt

--- BG_1.cpp
+++ BG_1.cpp
@@ -7,9 +7,28 @@ using namespace std;
 
 #define ll long long
 
+int n;	
+int a[1001];
+
+void Try(int i)
+{
+	for (size_t j = 0; j <= 1; j++)
+	{
+		a[i] = j;
+		if (i == n)
+		{
+			// In 
+		}
+		else
+		{
+			Try(i + 1);
+		}
+	}
+}
+
 int main()
 {
-
+	
 
 	return 0;
 }
