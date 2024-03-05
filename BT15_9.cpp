9795f5f586ec09d6d76587ded014b61d2a2835d8
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-03-05 19:37:56 +0700

    ADd

--- BT15_9.cpp
+++ BT15_9.cpp
@@ -21,7 +21,39 @@ int f[1001][1001];
 
 int main()
 {
+	int w[1001];
+	int v[1001];
+	int V;
 
+	cin >> n >> V;
+
+	for (size_t i = 1; i <= n; i++)
+	{
+		cin >> w[i];
+	}
+
+	for (size_t i = 1; i <= n; i++)
+	{
+		cin >> v[i];
+	}
+
+	int F[1001][1001];
+	F[0][0] = 0;
+
+	for (size_t i = 01; i <= n; i++)
+	{
+		for (size_t j = 1; j <= V; j++)
+		{
+			if (j >= w[i])
+			{
+				F[i][j] = max(v[i] + F[i - 1][j - w[i]], F[i - 1][j]);
+			}
+			else
+			{
+
+			}
+		}
+	}
 
 	return 0;
 }
