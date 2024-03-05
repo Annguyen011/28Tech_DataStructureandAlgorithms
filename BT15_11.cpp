aafa7080fcaadd8bfbcd7203cddb96ff3f508428
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-03-05 23:17:49 +0700

    Add

--- BT15_11.cpp
+++ BT15_11.cpp
@@ -21,7 +21,33 @@ int f[1001][1001];
 
 int main()
 {
+	cin >> n >> s;
 
+	for (size_t i = 1; i <= n; i++)
+	{
+		cin >> a[i];
+	}
+
+	for (size_t i = 1; i <= n; i++)
+	{
+		f[i][0] = 1;
+	}
+
+	for (size_t i = 1; i <= n; i++)
+	{
+		for (size_t j = 1; i <= n; i++)
+		{
+			if (j >= a[i])
+			{
+				f[i][j] = f[i - 1][j - a[i]] + f[i - 1][j];
+			}
+			else
+			{
+				f[i][j] = f[i - 1][j];
+			}
+			f[i][j] %= mod;
+		}
+	}
 
 	return 0;
 }
