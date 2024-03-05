5ede4cb7278f57ac3af518221cc96e035f227d09
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-03-05 14:37:12 +0700

    ADd

--- BT15_4.cpp
+++ BT15_4.cpp
@@ -18,25 +18,8 @@ int a[1001];
 int d[1001];
 int f[1001];
 
-
 int main()
 {
-	f[0] = 1;
-	f[1] = 1;
-
-	cin >> n >> k;
-
-	for (size_t i = 1; i <= n; i++)
-	{
-
-		for (size_t j = 1; j <= k; j++)
-		{
-			if (i - j > 0)
-			{
-				f[i] += f[i - j];
-			}
-		}
-	}
 
 
 	return 0;
