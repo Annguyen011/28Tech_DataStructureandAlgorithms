a7f72302c3344396a4d7f4c907a9fb3038b059c5
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-03-05 15:29:31 +0700

    ADd

--- BT15_6.cpp
+++ BT15_6.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 #define ll long long
 
+ll mod = 1e9 + 7;
 int n;
 int ok;
 int cnt;
@@ -16,6 +17,36 @@ int s;
 int x[1001];
 int a[1001];
 int d[1001];
+
+int Rev(int n)
+{
+	int s = 0;
+	while (n)
+	{
+		int r = n % 10;
+		s = s * 10 + r;
+		n /= 10;
+	}
+}
+
+ll PowMod(int a, int n)
+{
+	if (n == 0)
+	{
+		return 1;
+	}
+
+	ll x = PowMod(a, n / 2);
+
+	if (n % 2 == 0)
+	{
+		return x * x % mod;
+	}
+	else
+	{
+		return ((x * x % mod) * (a % mod)) % mod;
+	}
+}
 
 int main()
 {
---
+++ BT15_7.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+
+using namespace std;
+
+#define ll long long
+
+ll mod = 1e9 + 7;
+int n;
+int ok;
+int cnt;
+int k;
+int res = INT_MAX;
+int s;
+int x[1001];
+int a[1001];
+int d[1001];
+
+int main()
+{
+
+
+	return 0;
+}
