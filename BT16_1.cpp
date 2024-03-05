90dccb2bde58ff6b5d2b89c94323436296d791ac
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-03-05 23:36:35 +0700

    Add

--- BT16_1.cpp
+++ BT16_1.cpp
@@ -21,7 +21,20 @@ int f[1001][1001];
 
 int main()
 {
+	// Do thi co don do thi va da do thi
+	// Nam do thi lien thong
+	// Lien thong manh va lien thong yeu 
+	// Cac khai niem co ban tren do thi
 
+	// G la do thi
+	// V
+	// E
+
+	// Su dung ma tran ke de bieu dien
+	// Luon doi xung qua duong cheo chinh
+	// Ma tran trong so
+	// Hien thi thay vi 1 va 0 thi dung cac trong so de bieu dien
+	// 
 
 	return 0;
 }
