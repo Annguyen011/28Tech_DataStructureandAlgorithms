00e45bd2507fff325409d9af2068ce973320b44a
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-03-05 14:47:37 +0700

    ADd

--- BT15_5.cpp
+++ BT15_5.cpp
@@ -20,25 +20,7 @@ int f[1001];
 
 int main()
 {
-	cin >> n;
 
-	for (size_t i = 0; i < n; i++)
-	{
-		cin >> a[i];
-	}
-
-	for (size_t i = 0; i < n; i++)
-	{
-		d[a[i]] = 1;
-
-		for (size_t j = 0; j < i; j++)
-		{
-			if (a[i] > a[j])
-			{
-				d[a[i]] = max(d[a[j]] + 1, d[a[i]]);
-			}
-		}
-	}
 
 	return 0;
 }
