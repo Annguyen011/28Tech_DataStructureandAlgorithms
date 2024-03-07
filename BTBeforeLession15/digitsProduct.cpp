1efed31274e94e1d083cbcfd42c49470fd00f80d
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-03-07 18:21:17 +0700

    Add

--- BTBeforeLession15/digitsProduct.cpp
+++ BTBeforeLession15/digitsProduct.cpp
@@ -24,19 +24,4 @@ int d[1001];
 int f[1001][1001];
 vector<int> adj[1001];
 vector<int> tadj[1001];
-vector<pair<int, int >> dsCanh;
-bool visited[1001];
-stack<int> st;
 
-int lastDigitDiffZero(int n)
-{
-    long long res = 1;
-    for (int i = 2; i <= n; i++)
-    {
-        res *= i;
-        while (res % 10 == 0) res /= 10;
-        res = res % 100;
-    }
-    while (res % 10 == 0) res /= 10;
-    return res % 10;
-}
